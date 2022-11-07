2df9e6f037b45a36ebafa55fa527979b1030b856
Mastershifu <71121567+ouma-james@users.noreply.github.com>
2022-11-07 10:32:08 +0300

    Update Compute_game.java

--- Compute_game.java
+++ Compute_game.java
@@ -6,13 +6,7 @@ public class Question1 {
     public static void main(String[] args) {
 
         Scanner read=new Scanner(System.in);
-        //user keys the number of rounds to play-(variable)
-        //for each round user enters lucky number-(variable/for loop)
-        //divide the numbers with a secret number-(create a constant secret value)
-        //if remainder is zero total score increment by 1-(increment operator)
-        //if the score is any other even number increment score by 3-(check if even)
-        //if odd number decrement total score by 3(check if odd)
-
+     
         int lucky_number,play_times,remainder;
         int total_score=0;
         final int secret_number=4;
